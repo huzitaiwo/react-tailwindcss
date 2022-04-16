d80b65e1e6f7e1783cc9379eeec309d6f05856c5
huzitaiwo <taiwohusayn5@gmail.com>
2022-04-16 18:15:57 +0100

    installed firebase

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      app
+      <h1 className="text-6xl font-bold underline">
+        Hello world!
+      </h1>
     </div>
   );
 }
