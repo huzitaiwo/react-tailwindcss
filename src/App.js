44563a05a4937eb9ef2eda98a1e443b2d9ddbf78
huzitaiwo <taiwohusayn5@gmail.com>
2022-04-16 21:07:36 +0100

    template

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className="text-6xl font-bold underline">
+      <h1 className="text-6xl font-bold underline bg-gray-500">
         Hello world!
       </h1>
     </div>
