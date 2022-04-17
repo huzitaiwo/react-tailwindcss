d2b3a6e18155709a47e81adae5c2f502b12ae45f
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-04-17 18:47:26 +0100

    added more Routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-// styles
+// styles and components
 import './App.css';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+import About from './pages/About'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <main>
+          <div></div>
           <Switch>
             <Route exact path='/'>
               <Home />
+            </Route>
+            <Route path='/contact'>
+              <Contact />
+            </Route>
+            <Route path='/about'>
+              <About />
             </Route>
             <Route path='/signup'>
               <Signup />
