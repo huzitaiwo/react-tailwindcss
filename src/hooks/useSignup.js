d5abd62f30d404f13c9345bd4cb1a1e99c76efb2
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-04-18 01:54:41 +0100

    creating signup hook

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -5,17 +5,24 @@ export const useSignup = () => {
   const [error, setError] = useState(null)
   const [isPending, setIsPending] = useState(false)
 
-  const signup = async (email, password, disPlayName) => {
+  const signup = async (email, password, displayName) => {
     setError(null)
     setIsPending(true)
 
     try {
+      //sign user up
       const res = await firebaseAuth.createUserWithEmailAndPassword(email, password)
       console.log(res.user)
 
       if (!res) {
         throw new Error('Colud not complete signup')
       }
+
+      //add displayName to user
+      await res.user.updateProfile({ displayName })
+
+      setIsPending(false)
+      setError(null)
     }
     catch (err) {
       console.log(err.message)
