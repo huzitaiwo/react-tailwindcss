85ac63c2d61c247d0872996dbc0fdd276f4a593d
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-04-18 04:08:13 +0100

    dispatched a login action

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,7 +1,10 @@
 import { useState } from 'react'
 import { firebaseAuth, firebaseStorage } from '../firebase/config'
+import { useAuthContext } from './useAuthcontext'
 
 export const useSignup = () => {
+  const { dispatch } = useAuthContext()
+
   const [error, setError] = useState(null)
   const [isPending, setIsPending] = useState(false)
 
@@ -10,16 +13,18 @@ export const useSignup = () => {
     setIsPending(true)
 
     try {
-      //sign user up
+      // sign user up
       const res = await firebaseAuth.createUserWithEmailAndPassword(email, password)
-      console.log(res.user)
 
       if (!res) {
         throw new Error('Colud not complete signup')
       }
 
-      //add displayName to user
+      // add displayName to user
       await res.user.updateProfile({ displayName })
+
+      // dispatch login action
+      dispatch({ type: 'LOGIN', payload: res.user })
 
       setIsPending(false)
       setError(null)
