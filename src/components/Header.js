adfcc0736e07fba3cb4ed597f42768b39b8a322e
Im-Hassan-wd <alhassansahih@gmail.com>
2022-04-19 22:29:34 +0100

    created Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 export default function Header() {
   return (
-    // <header className="flex justify-center md:justify-end mb-5">
-    //   <Link className="btn text-gray-600 hover:bg-gray-600 hover:text-white md:border transition ease-in-out duration-500" to='/login'>Login</Link>
-    //   <Link className="btn text-gray-600 hover:bg-gray-600 hover:text-white mx-2 md:border transition ease-in-out duration-500" to='/signup'>Signup</Link>
-    //   {!isPending && <button onClick={logout} className="btn text-gray-600 hover:bg-gray-600 hover:text-white mx-2 md:border transition ease-in-out duration-500">Logout</button>}
-    //   {isPending && <button onClick={logout} className="btn text-gray-600 hover:bg-gray-600 hover:text-white mx-2 md:border transition ease-in-out duration-500">Loging out...</button>}
-    // </header>
+    <header className="flex justify-center md:justify-end mb-5">
+      <Link className="btn text-gray-600 hover:bg-gray-600 hover:text-white md:border transition ease-in-out duration-500" to='/login'>Login</Link>
+      <Link className="btn text-gray-600 hover:bg-gray-600 hover:text-white mx-2 md:border transition ease-in-out duration-500" to='/signup'>Signup</Link>
+      {!isPending && <button onClick={logout} className="btn text-gray-600 hover:bg-gray-600 hover:text-white mx-2 md:border transition ease-in-out duration-500">Logout</button>}
+      {isPending && <button onClick={logout} className="btn text-gray-600 hover:bg-gray-600 hover:text-white mx-2 md:border transition ease-in-out duration-500">Loging out...</button>}
+    </header>
   )
 }
