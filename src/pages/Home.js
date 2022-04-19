8b06f0ad5edc4bd860a96414876e144c206ac21d
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-04-19 04:39:20 +0100

    adding icons

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -40,7 +40,12 @@ export default function Home() {
               <span className="font-bold block">Ved Noodles</span>
               <span>Recipe by Luigi</span>
             </div>
-            <div className="badge">25mins</div>
+            <div className="badge">
+              <svg xmlns="http://www.w3.org/2000/svg" className="w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                <path strokeLinecap="round" strokeLinejoin="round" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
+              </svg>
+              <span>25mins</span>
+            </div>
           </div>
         </div>
 
@@ -63,7 +68,7 @@ export default function Home() {
         </div>
 
         <div className="flex justify-center">
-          <button className="btn">Load more</button>
+          <button className="btn bg-gray-500 text-white">Load more</button>
         </div>
       </div>
     </>
