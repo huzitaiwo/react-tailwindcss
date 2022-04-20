7f16f87ab34beea825a996ab90988e7fc2fa8c5c
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-04-20 21:09:59 +0100

    login user out

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,14 @@ import { useLogout } from '../hooks/useLogout'
 
 
 export default function Header() {
-  const { logout, isPending, error } = useLogout()
+  const { logout, isPending } = useLogout()
 
   return (
     <header className="flex justify-center md:justify-end mb-5">
       <Link to='/login' className="btn text-gray-600 hover:bg-gray-600 hover:text-white md:border transition ease-in-out duration-500">Login</Link>
       <Link to='/signup' className="btn text-gray-600 hover:bg-gray-600 hover:text-white mx-2 md:border transition ease-in-out duration-500">Signup</Link>
-      {!isPending && <button className="btn text-gray-600 hover:bg-gray-600 hover:text-white mx-2 md:border transition ease-in-out duration-500">Logout</button>}
-      {isPending && <button onClick={logout} className="btn text-gray-600 hover:bg-gray-600 hover:text-white mx-2 md:border transition ease-in-out duration-500">Loging out...</button>}
+      {isPending && <button className="btn text-gray-600 hover:bg-gray-600 hover:text-white mx-2 md:border transition ease-in-out duration-500">Loging out...</button>}
+      {!isPending && <button onClick={logout} className="btn text-gray-600 hover:bg-gray-600 hover:text-white mx-2 md:border transition ease-in-out duration-500">Logout</button>}
     </header>
   )
 }
