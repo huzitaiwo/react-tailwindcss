e8b446fe419efc453ec9f5c1410b13865d51617a
Im-Hassan-wd <alhassansahih@gmail.com>
2022-04-20 03:43:27 +0100

    creatng useLogin hook

---
+++ src/hooks/useLogin.js
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react'
+import { firebaseAuth } from '../firebase/config'
+import { useAuthContext } from './useAuthContext'
+
+export const useLogin = (email, password) => {
+  const { dispatch } = useAuthContext()
+  
+  const [error, setError] = useState(null)
+  const [isPending, setIsPending] = useState(false)
+  const [unMounted, setUnMounted] = useState(false)
+
+  const login = async () => {
+    setError(null)
+    setIsPending(true)
+
+    // sign user out
+    try {
+      const res = await firebaseAuth.signInWithEmailAndPassword(email, password)
+
+      //dispatch login action
+      dispatch({ type: 'LOGIN' })
+
+      // update states
+      if (!unMounted) {
+        setIsPending(false)
+        setError(null)
+      }
+    }
+    
+    catch(err) {
+      if (!unMounted) {
+        setIsPending(false)
+        setError()
+      }
+    }
+  }
+
+  useEffect(() => {
+    return () => setUnMounted(true)
+  }, [])
+
+  return { login, error, isPending }
+
+}
