ee943d9a4682e35e34c4a7c21bd1ee3667cde778
Im-Hassan-wd <alhassansahih@gmail.com>
2022-04-20 05:02:06 +0100

    created login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,54 @@
+import { useState } from "react"
+import { useLogin } from '../hooks/useLogin'
+
 export default function Login() {
+  const { login, error, isPending } = useLogin()
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
-    <form>
-      
-    </form>
+    <div>
+      <Header />
+      <div className='flex justify-center'>
+        <form onSubmit={handleSignup}>
+          <h2 className='text-2xl font-bold mb-4'>Login</h2>
+
+          <label className='block mb-5 w-full'>
+            <span className='block mb-2'>email:</span>
+            <input className='block px-2 py-1' 
+              type="email"
+              onChange={e => setEmail(e.target.value)}
+              value={email}
+            />
+          </label>
+          <label className='block mb-5'>
+            <span className='block mb-2'>password:</span>
+            <input className='px-2 py-1' 
+              type="password"
+              onChange={e => setPassword(e.target.value)}
+              value={password}
+            />
+          </label>
+          <label className='block mb-5'>
+            <span className='block mb-2'>display name:</span>
+            <input className='px-2 py-1' 
+              type="text"
+              onChange={e => setDisplayName(e.target.value)}
+              value={displayName}
+            />
+          </label>
+          <label className='block mb-5'>
+            <span className='block mb-2'>profile thumbnail:</span>
+            <input className='px-2 py-1' 
+              type="file"
+              onChange={handleFileChange}
+            />
+            {thumbnailError && <div className='error'>{thumbnailError}</div>}
+          </label>
+          <button className="mt-5 btn bg-gray-600 hover:bg-gray-800 text-white hover:shadow-inner transform hover:scale-125 hover:bg-opacity-50 transition ease-out duration-300">signup</button>
+        </form>
+      </div>
+    </div>
   )
 }
