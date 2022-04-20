a5217f7fbc0cb5189542c7ae379a21f00c31aed5
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-04-20 22:02:40 +0100

    used authIsReady to render App componenets

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { useAuthContext } from './hooks/useAuthContext'
 
 // styles and components
 import './App.css';
@@ -11,22 +12,26 @@ import Navbar from './components/Navbar'
 
 
 function App() {
+  const { authIsReady } = useAuthContext()
+
   return (
     <div className="App font-poppins text-gray-600">
-      <div className='grid md:grid-cols-3'>
-        <BrowserRouter>
-          <Navbar />
-          <main className='px-6 md:px-14 py-6 bg-gray-100 md:col-span-2 min-h-screen'>
-            <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/about' element={<About />} />
-              <Route path='/contact' element={<Contact />} />
-              <Route path='/signup' element={<Signup />} />
-              <Route path='/login' element={<Login />} />
-            </Routes>
-          </main>
-        </BrowserRouter>
-      </div>
+      {authIsReady && (
+        <div className='grid md:grid-cols-3'>
+          <BrowserRouter>
+            <Navbar />
+            <main className='px-6 md:px-14 py-6 bg-gray-100 md:col-span-2 min-h-screen'>
+              <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/contact' element={<Contact />} />
+                <Route path='/signup' element={<Signup />} />
+                <Route path='/login' element={<Login />} />
+              </Routes>
+            </main>
+          </BrowserRouter>
+        </div>
+      )}
     </div>
   );
 }
