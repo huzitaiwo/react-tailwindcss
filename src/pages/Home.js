287f42717cff480238d499fcd91ea7c086c6e204
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-04-21 11:08:30 +0100

    adding more meals

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,6 +7,12 @@ import bangersMash from  '../assets/bangers_and_mash.jpg'
 import chessBurger from '../assets/cheeseburger.jpg'
 import chichagoPizza from '../assets/chicago-style_pizza.jpg'
 import cottagePie from '../assets/cottage_pie.jpg'
+import delmonicoSteak from '../assets/src/assets/Delmonico_steak.jpg'
+import fishChips from '../assets/fish_and_chips.jpg'
+import freshSalad from '../assets/fresh_salad_and_appetizers.jpg'
+import englishBreakefast from '../assets/full_english_breakfast.jpg'
+import grilledVegetables from '../assets/grilled_vegetables_with_mushrooms.jpg'
+import mapleGlazedTurkey from '../assets/maple_glazed_turkey_dinner.jpg'
 
 //components
 import Card from '../components/Card'
@@ -39,10 +45,10 @@ export default function Home() {
         <div className="mt-6 grid lg:grid-cols-2 xl:grid-cols-3 gap-5">
           {/* card goes here */}
           <Card src={bakedSalmon} name='Baked Salmon with cherry tomatoes' />
-          <Card src={curry} />
-          <Card src={curry} />
-          <Card src={curry} />
-          <Card src={curry} />
+          <Card src={bangersMash} name='Bangers and Mash' />
+          <Card src={chessBurger} name='Cheese Burger' />
+          <Card src={chichagoPizza} name='Chichago Pizza' />
+          <Card src={cottagePie} name='Cottage Pie' />
           <Card src={curry} />
           <Card src={curry} />
           <Card src={curry} />
